7ae7dada9bccb2ad4bdaca8018d4be43447d118d
Nikos Kyriakopoulos <dummy@nikos.com>
2023-05-06 00:33:02 +0300

    added simple fight logging with uids, no ELO calcs

--- api_testing.py
+++ api_testing.py
@@ -8,7 +8,19 @@ data = {
   "first_name": "iita",
   "last_name": "Lehtinen",
 }
-add_competitor_response = requests.post(add_competitor_URL, json=data)
+#add_competitor_response = requests.post(add_competitor_URL, json=data)
 
-print(add_competitor_response.status_code)
-print(add_competitor_response.text)
+#print(add_competitor_response.status_code)
+#print(add_competitor_response.text)
+
+
+new_fight_URL = "http://127.0.0.1:8000/new_fight"
+
+data = {
+  "winner_uid": "0684b247ba5a4eea9bea958562242de6",
+  "loser_uid": "758498fdedf74f4aaf0b554212e9fd88",
+}
+new_fight_response = requests.post(new_fight_URL, json=data)
+
+print(new_fight_response.status_code)
+print(new_fight_response.text)
--- models.py
+++ models.py
@@ -10,7 +10,7 @@ from database import Base
 class Competitor(Base):
     '''class for the fighters table in the database'''
     __tablename__ = "competitors"
-    default_uid = uuid.uuid4
+    default_uid = uuid.uuid4()
 
     competitor_uid = Column(UUID(as_uuid=True), primary_key=True, default=default_uid)
     first_name = Column(String)
@@ -22,7 +22,9 @@ class Competitor(Base):
 class Fight(Base):
     '''class for the fights table in the database'''
     __tablename__ = "fights"
-    fight_uid = Column(String, primary_key=True)
-    winner = Column(String, ForeignKey("competitors.competitor_uid"))
-    loser = Column(String, ForeignKey("competitors.competitor_uid"))
+    default_uid = uuid.uuid4()
+
+    fight_uid = Column(UUID(as_uuid=True), primary_key=True, default=default_uid)
+    winner_uid = Column(UUID(as_uuid=True), ForeignKey("competitors.competitor_uid"))
+    loser_uid = Column(UUID(as_uuid=True), ForeignKey("competitors.competitor_uid"))
     timestamp = Column(DateTime(timezone=True), default=datetime.now)
